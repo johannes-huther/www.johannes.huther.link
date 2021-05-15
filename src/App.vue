d210b46f7eb4f470ee8dd1046fe77eb2f1ef2b40
Johannes Huther <dev.johannes@huther.link>
2021-05-15 17:18:36 +0200

    Use Vue framework.
    Using a web framework might seem unnecessary at the moment but might safe much time later on.
    
    Vuetify is available for material design.

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <header style="display: flex">
+      <img
+        class="centered"
+        src="../img/johannes-huther-icon.svg"
+        width="128px"
+        alt="Icon of Johannes Huther"
+      />
+    </header>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+/**
+ * The main {@link Vue}-View that contains the App.
+ */
+@Component({
+  metaInfo: {
+    titleTemplate: "%s - Johannes Huther",
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.v-application a {
+  margin: 10px;
+  color: var(--v-accent);
+  &.router-link-exact-active {
+    text-decoration: none;
+  }
+}
+
+.centered {
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="display: flex; flex-direction: column">
+    <p class="content centered">
+      This is the personal website of Johannes Huther.
+    </p>
+    <p class="content centered">More content coming soon.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  metaInfo: {
+    title: "Home",
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.centered {
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
